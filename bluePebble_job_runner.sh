cc604b1752d30b9cd9ec352e52d3e57b79203406
JosephTrevorrow <joseph.trevorrow@gmail.com>
2024-09-23 16:18:12 +0100

    Update bluePebble_job_runner.sh

--- bluePebble_job_runner.sh
+++ bluePebble_job_runner.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#SBATCH --job-name=ia23938-iai-cdt-job
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1:0:0
+#SBATCH --mem=64G
+#SBATCH --account=COSC023424
+
 # Define the Python file you want to run
 PYTHON_FILE="/home/ia23938/Documents/GitHub/ValueSystemsAggregation/experiment_runner.py"
 
