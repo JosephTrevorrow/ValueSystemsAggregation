1493f18024e126fe5d5a6d5649a58c7c50346027
JosephTrevorrow <joseph.trevorrow@gmail.com>
2024-09-24 15:18:13 +0100

    adding experiment runner

--- bluePebble_job_runner.sh
+++ bluePebble_job_runner.sh
@@ -3,11 +3,27 @@
 #SBATCH --job-name=ia23938-iai-cdt-job
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0:0:30
-#SBATCH --mem=100M
+#SBATCH --time=0:10:0
+#SBATCH --mem=1G
 #SBATCH --account=cosc023424
 
 PYTHON_FILE="experiment_runner.py"
 
-python3 -m pip install -r requirements.txt
-python3 $PYTHON_FILE
+cd "${SLURM_SUBMIT_DIR}"
+
+echo Running on host "$(hostname)"
+echo Time is "$(date)"
+echo Directory is "$(pwd)"
+echo Slurm job ID is "${SLURM_JOBID}"
+echo This jobs runs on the following machines:
+echo "${SLURM_JOB_NODELIST}"
+
+module add gcc/12.3.0
+
+source ~/ValueSystemsAggregation/initConda.sh
+
+conda activate new_env
+
+python -m pip install -r requirements.txt
+python $PYTHON_FILE
+
