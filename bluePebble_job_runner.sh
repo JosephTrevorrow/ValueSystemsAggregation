7fcabf3144f8e574876618b8e3a8c1f07f71b515
JosephTrevorrow <joseph.trevorrow@gmail.com>
2024-09-24 09:32:40 +0100

    debug bluepebble

--- bluePebble_job_runner.sh
+++ bluePebble_job_runner.sh
@@ -3,11 +3,10 @@
 #SBATCH --job-name=ia23938-iai-cdt-job
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=1:0:0
-#SBATCH --mem=64G
-#SBATCH --account=COSC023424
+#SBATCH --time=0:0:30
+#SBATCH --mem=100M
+#SBATCH --account=cosc023424
 
-# Define the Python file you want to run
-PYTHON_FILE="/home/ia23938/Documents/GitHub/ValueSystemsAggregation/experiment_runner.py"
+PYTHON_FILE="user/home/ia23938/ValueSystemsAggregation/experiment_runner.py"
 
 python3 $PYTHON_FILE
